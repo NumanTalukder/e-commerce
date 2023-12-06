8f612e7ecaea8ce0b3f1da4c3c1b9ffca7981bb8
NumanTalukder <51156089+NumanTalukder@users.noreply.github.com>
2023-12-06 11:17:06 +0600

    product reviews

--- src/app/product/[product]/page.tsx
+++ src/app/product/[product]/page.tsx
@@ -1,6 +1,7 @@
 import Container from '@/components/Container'
 import { product } from '@/utils/product'
 import ProductDetail from '@/components/ProductDetail'
+import ListRating from '@/components/Products/ListRating'
 
 interface iParams {
   params: string
@@ -13,6 +14,7 @@ const Product = ({ params }: { params: iParams }) => {
     <div>
       <Container>
         <ProductDetail product={product} />
+        <ListRating product={product} />
       </Container>
     </div>
   )
